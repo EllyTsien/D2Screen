97415f3b47ca7979fa97f4a66814a6efc1bca977
qti <go73yow@tum.de>
2024-10-18 02:34:14 +0800

    add SMILES_transfer

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit import RDLogger
 import pickle as pkl
-import paddle.nn as nn
+from threading import Thread, Lock
+from pahelix.utils.compound_tools import mol_to_geognn_graph_data_MMFF3d
+
 
 class Input_ligand_preprocess:
     # def __init__(self, train_file, test_file):
@@ -57,7 +60,99 @@ class Input_ligand_preprocess:
         self.save_processed_data()
         self.save_smiles_list()
 
+
+class SMILES_Transfer:
+    def __init__(self, train_df):
+    #def __init__(self, train_df, test_df):
+        self.train_df = train_df
+        # self.test_df = test_df
+        self.mutex = Lock()
+        self.p = 0
+        self.smiles_to_graph_dict = {}
+
+    def calculate_3D_structure(self, smiles_list):
+        n = len(smiles_list)
+        index = 0
+        while True:
+            self.mutex.acquire()
+            if self.p >= n:
+                self.mutex.release()
+                break
+            index = self.p
+            smiles = smiles_list[index]
+            print(index, ':', round(index / n * 100, 2), '%', smiles)
+            self.p += 1
+            self.mutex.release()
+            
+            try:
+                molecule = AllChem.MolFromSmiles(smiles)
+                molecule_graph = mol_to_geognn_graph_data_MMFF3d(molecule)
+            except Exception as e:
+                print("Invalid smiles!", smiles)
+                self.mutex.acquire()
+                with open('work/invalid_smiles.txt', 'a') as f:
+                    f.write(str(smiles) + '\n')
+                self.mutex.release()
+                continue
+
+            self.mutex.acquire()
+            self.smiles_to_graph_dict[smiles] = molecule_graph
+            self.mutex.release()
+
+    def process_smiles(self, mode, thread_count=12):
+        if mode == 'train':
+            print(type(self.train_df))
+
+            smiles_list = self.train_df["SMILES"].tolist()
+
+
+        self.smiles_to_graph_dict = {}
+        self.p = 0
+
+        threads = []
+        for _ in range(thread_count):
+            threads.append(Thread(target=self.calculate_3D_structure, args=(smiles_list,)))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+        pkl.dump(self.smiles_to_graph_dict, open(f'work/{mode}_smiles_to_graph_dict.pkl', 'wb'))
+        print(f'{mode} is Done!')
+
+    def save_data_list(self):
+        train_smiles_to_graph_dict = pkl.load(open('work/train_smiles_to_graph_dict.pkl', 'rb'))
+        # test_smiles_to_graph_dict = pkl.load(open('work/test_smiles_to_graph_dict.pkl', 'rb'))
+
+        train_data_list = []
+        # test_data_list = []
+
+        for index, row in self.train_df.iterrows():
+            smiles = row["SMILES"]
+            label = row["label"]
+            if smiles not in train_smiles_to_graph_dict:
+                continue
+            data_item = {
+                "smiles": smiles,
+                "graph": train_smiles_to_graph_dict[smiles],
+                "label": label,
+            }
+            train_data_list.append(data_item)
+
+
+        pkl.dump(train_data_list, open('work/train_data_list.pkl', 'wb'))
+        # pkl.dump(test_data_list, open('work/test_data_list.pkl', 'wb'))
+        print('Data lists have been saved!')
+
+    def run(self):
+        for mode in ['train']:
+            self.process_smiles(mode)
+        self.save_data_list()
+
 # 使用示例
 if __name__ == "__main__":
     processor = Input_ligand_preprocess('datasets/input.csv')
     processor.preprocess()
+    processed_input_csv = pd.read_csv('datasets/train_preprocessed.csv')
+    SMILES_Transfer = SMILES_Transfer(processed_input_csv)
+    SMILES_Transfer.run()
