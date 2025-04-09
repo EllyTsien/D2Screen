b0707812fd2367277a51edb46132ea068077afd7
tingli_qian <121090453@link.cuhk.edu.cn>
2025-04-09 17:01:46 +0800

    solve bug

--- prediction_ZINC.py
+++ prediction_ZINC.py
@@ -37,60 +37,6 @@ def main(args):
             raise FileNotFoundError(f"The file '{input_ligands_path}' does not exist.")
         processed_input_path = 'datasets/train_preprocessed.csv'
 
-    thread_num =  int( args.thread_num )
-
-
-    # process train dataset
-    processor = Input_ligand_preprocess(input_ligands_path, project_name)
-    processor.preprocess() 
-    processed_input_csv = pd.read_csv(processed_input_path)
-    SMILES_transfer = SMILES_Transfer(processed_input_csv)
-    SMILES_transfer.run()
-
-    
-    model_config_list = ["mlp4", "mlp6"]
-    lrs_list = [
-        ("1e-3", "1e-3"),
-        ("5e-3", "5e-3"),
-        ("1e-4", "1e-3"),
-        ("0", "1e-3"),
-        ("0", "5e-3"),
-        ("0", "1e-3")
-
-    ]
-    drop_list = [0.2, 0.5]
-    batch_size_list = [32,256,1024]
-    
-    '''
-    model_config_list = ["mlp4"]
-    lrs_list = [
-        ("1e-3", "1e-3"),
-        ("0", "1e-3")
-
-    ]
-    drop_list = [0.2]
-    batch_size_list = [32,128]
-    '''
-
-    # 创建参数组合
-    tasks = []
-    for finetune_model_config in model_config_list:
-        for lr, head_lr in lrs_list:
-            for dropout_rate in drop_list:
-                for batch_size in batch_size_list:
-                    for ft_time in range(1, 2):
-                        tasks.append((finetune_model_config, lr, head_lr, dropout_rate, ft_time, batch_size, project_name, finetune_dataset, model_version))
-    
-    start_time = time.time()  # 记录开始时间
-    # 并行处理任务
-    with Pool(thread_num) as p:
-        p.map(run_finetune, tasks)
-    end_time = time.time()  # 记录结束时间
-    elapsed_time = end_time - start_time
-    print(f"Total execution time for finetuning: {elapsed_time:.2f} seconds")
-
-    select_best_model(model_version, project_name)
-
     
     #first-stage screen of ZINC20 library
     for index in range(1, 23): 
