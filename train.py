83278288401ae685434e631610e535cc359e10d6
tingli_qian <121090453@link.cuhk.edu.cn>
2025-04-10 22:27:05 +0800

    solve mo.set_start_method()

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ def main(args):
     '''
 
 if __name__ == '__main__':
-    mp.set_start_method('spawn')
+    mp.set_start_method('spawn', force=True)
     parser = ArgumentParser()
     parser.add_argument('--project_name', default='finetune', type=str, help='Name your project on the wandb website')
     parser.add_argument('--dataset', default='input.csv', type=str, help='Choose dataset (required)')
