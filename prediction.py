06a494dceac38d1613a041b9efa0016ed549f2b2
EllyTsien <tingliqian2002@gmail.com>
2025-05-17 16:15:59 +0800

    update prediction

--- prediction.py
+++ prediction.py
@@ -1,34 +1,83 @@
-import paddle.nn.functional as F
-import paddle as pdl
+import os
+import time
+from multiprocessing import Pool
+import multiprocessing as mp
+from argparse import ArgumentParser
 import pandas as pd
+from dataloader import sort_and_filter_csv
+from finetune import test_DUDE, select_best_model,test, test_ratio
+from preprocess import Input_ligand_preprocess,  SMILES_Transfer
 
-class ModelTester:
-    def __init__(self, model_class, data_loader_func, model_version):
-        self.model_class = model_class
-        self.data_loader_func = data_loader_func
-        self.model_version = model_version
-        self.model = self.load_model()
-        self.test_data_loader = self.data_loader_func(mode='test', batch_size=1024)
+model_version = '1'
+
+def main(args):
+    # passing parameters
+    if args.project_name is None:
+        print("Using default project name, finetune")
+        project_name = "finetune"
+    else:
+        project_name = args.project_name
+
+    if args.dataset is None:
+        finetune_dataset = args.dataset
+        print("Using default dataset, input.csv")
+        input_ligands_path = 'datasets/input.csv'
+        print(f"Using dataset: {input_ligands_path}")
+        if not os.path.exists(input_ligands_path):
+            raise FileNotFoundError(f"The file '{input_ligands_path}' does not exist.")
+        # processed_input_path = 'datasets/train_preprocessed.csv'
+    else:
+        finetune_dataset = args.dataset
+        input_ligands_path = 'datasets/' + args.dataset
+        print(f"Using dataset: {input_ligands_path}")
+        if not os.path.exists(input_ligands_path):
+            raise FileNotFoundError(f"The file '{input_ligands_path}' does not exist.")
+        # processed_input_path = 'datasets/train_preprocessed.csv'
+
+    if args.mode is None:
+        print("Using default mode, DUDE")
+        raise ValueError("Please specify a mode: ZINC or DUDE or ratio")
+    else:   
+        mode = args.mode
+        print(f"Using mode: {mode}")
+        if mode not in ['ZINC', 'DUDE', 'ratio']:
+            raise ValueError("Invalid mode. Please choose from ZINC, DUDE, or ratio.")
+
+    select_best_model(model_version, project_name)
+    
+    #preprocess DUDE/test_nolabel.csv to .pkl
+    processor = Input_ligand_preprocess(input_ligands_path, project_name, mode)
+    processed_input_path = processor.preprocess() 
+    processed_input_csv = pd.read_csv(processed_input_path)
+    SMILES_transfer = SMILES_Transfer(processed_input_csv, project_name, mode)
+    SMILES_transfer.run()
+    print(processed_input_path)
+    if mode == 'ZINC':
+        #first-stage screen of ZINC20 library
+        for index in range(1, 10): 
+            test(model_version='1', project_name=project_name, index=index)
+        # Sort, filter and log the final result
+        sort_and_filter_csv(project_name + '/ZINC20/ZINC20_DL_result.csv', args.threshold, project_name + '/ZINC20/DL_top.csv')
+    elif mode == 'DUDE':    
+        test_DUDE(model_version='1', project_name=project_name, nolabel_file_path= processed_input_path, index=0)
+        # Sort, filter and log the final result
+        sort_and_filter_csv(project_name + '/DL_DUDE_result.csv', args.threshold, project_name + '/DL_DUDE_top.csv')
+    elif mode == 'ratio':
+        test_ratio(model_version='1', project_name=project_name, nolabel_file_path= processed_input_path, index=0)
+        # Sort, filter and log the final result
+        sort_and_filter_csv(project_name + '/DL_result.csv', args.threshold, project_name + '/DL_top.csv')
         
-    def load_model(self):
-        model = self.model_class()
-        model.set_state_dict(pdl.load("weight/" + self.model_version + ".pkl"))
-        model.eval()
-        return model
 
-    def run_test(self):
-        all_result = []
-        for atom_bond_graph, bond_angle_graph, label_true_batch in self.test_data_loader:
-            label_predict_batch = self.model(atom_bond_graph, bond_angle_graph)
-            label_predict_batch = F.softmax(label_predict_batch)
-            result = label_predict_batch[:, 1].cpu().numpy().reshape(-1).tolist()
-            all_result.extend(result)
 
-        df = pd.read_csv('data/data221048/test_nolabel.csv')
-        df['pred'] = all_result
-        df.to_csv('result.csv', index=False)
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    parser = ArgumentParser()
+    parser.add_argument('--project_name', default='finetune', type=str, help='Name your project on the wandb website')
+    parser.add_argument('--mode', default=None, type=str, help='Select mode: ZINC or DUDE or ratio')
+    parser.add_argument('--dataset', default='input.csv', type=str, help='Choose dataset (required)')
+    parser.add_argument('--n_samples', default=-1, type=int, help='Number of samples (default: all)')
+    parser.add_argument('--threshold', default=0.9, type=float, help="Threshold for predict value (defalt 0.9)")
+    parser.add_argument('--thread_num', default=1, type=int, help='Number of thread used for finetuning (default: 1)')
+    args = parser.parse_args()
 
-# Example usage:
-# Assuming ADMET is your model class and get_data_loader is your data loader function
-# model_tester = ModelTester(model_class=ADMET, data_loader_func=get_data_loader, model_version='1')
-# model_tester.run_test()
+    main(args)
